25ccbc11c3a2a843c2c515ccfd726caf1cf0f06d
Tomas Valent <equivalent@eq8.eu>
2015-10-20 23:18:36 +0100

    don't create event if POST body is empty

--- spec/requests/api/v1/events_spec.rb
+++ spec/requests/api/v1/events_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "API V1 Events", type: :request do
   describe "POST /events" do
     let(:trigger) { post api_v1_events_path, body }
 
-    context 'given body contains valid JSON' do
+    context 'given post body contains valid JSON' do
       let(:body) { ExampleFiles.json_example('valid_mandril_event') }
 
       it "should respond 201 created" do
@@ -18,6 +18,20 @@ RSpec.describe "API V1 Events", type: :request do
           .by 1
 
         expect(Event.last.raw).to eq body
+      end
+    end
+
+    context 'given post body is empty' do
+      let(:body) { nil }
+
+      it "should respond 400 bad request" do
+        trigger
+        expect(response).to have_http_status(400)
+      end
+
+      it "should not create event" do
+        expect { trigger }
+          .not_to change { Event.count }
       end
     end
   end
